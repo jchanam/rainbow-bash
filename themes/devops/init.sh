fae126ce95f16576c319ed1c464cf5763867fc34
Julio Chana <julio@spotahome.com>
2017-09-18 16:26:32 +0200

    Only show git icon if inside git repo

--- themes/devops/init.sh
+++ themes/devops/init.sh
@@ -20,9 +20,9 @@ get_git_color(){
 get_git_info(){
     if [[ $rbw_git_is_repo == 1 ]]; then
         if [[ $rbw_git_dirty == 1 ]]; then
-            echo -en "$rbw_git_branch "
+            echo -en " $rbw_git_branch "
         else
-            echo -en "$rbw_git_branch "
+            echo -en " $rbw_git_branch "
         fi
     fi
 }
@@ -34,7 +34,7 @@ get_k8s_info(){
 RBW_THEME="\
 ┌\[$RBW_R_BLUE\][\$(get_k8s_info)] \
 \[$RBW_RESET_ALL\]\w \
-\[\$(get_git_color)\] \$(get_git_info) \
+\[\$(get_git_color)\]\$(get_git_info) \
 \[$RBW_R_RED\]\[\033[\$((COLUMNS-10))G\] [\t]
 \[$RBW_RESET_ALL\]└\$ "
 
